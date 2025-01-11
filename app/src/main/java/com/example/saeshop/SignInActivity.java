730d01e6d141944d8c69faf20ddf43c7c73fec1b
Riki220999 <setiyoriki589@gmail.com>
2025-01-11 22:50:38 +0700

    Feat: added intent login page to home

--- app/src/main/java/com/example/saeshop/SignInActivity.java
+++ app/src/main/java/com/example/saeshop/SignInActivity.java
@@ -1,19 +1,39 @@
 package com.example.saeshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText inputUsername, inputPassword;
+    private Button loginButton;
+
+    Intent intentInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.signin_view);
+
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleLogin();
+            }
+        });
+    }
+
+    private void handleLogin() {
+        Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+        SignInActivity.this.startActivity(intent);
     }
 }
