b7accc16dd6e26feb8cdc04337329b24ff366587
Riki220999 <setiyoriki589@gmail.com>
2025-01-12 20:37:26 +0700

    Feat: action logout

--- app/src/main/java/com/example/saeshop/ProfileActivity.java
+++ app/src/main/java/com/example/saeshop/ProfileActivity.java
@@ -1,15 +1,33 @@
 package com.example.saeshop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    private Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.profile_view);
+
+        logoutBtn = findViewById(R.id.logoutBtn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleLogout();
+            }
+        });
+    }
+
+    private void handleLogout() {
+        Intent intent = new Intent(ProfileActivity.this, SignInActivity.class);
+        ProfileActivity.this.startActivity(intent);
     }
 }
